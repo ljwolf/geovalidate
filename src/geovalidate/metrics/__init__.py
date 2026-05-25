153a4228e9d360b4e0ff5fede8a7b9c885a6dcbc
Levi John Wolf <levi.john.wolf@gmail.com>
2026-05-25 12:06:24 +0100

    add esda-based stats to metric module

--- src/geovalidate/metrics/__init__.py
+++ src/geovalidate/metrics/__init__.py
@@ -1,5 +1,14 @@
 """Metrics for evaluating geovalidate sampler and CV outputs."""
 
 from ._aoa import area_of_applicability
+from esda import (
+    completeness,
+    homogeneity,
+    areal_entropy,
+    overlay_entropy,
+    correlogram,
+    boundary_silhouette,
+    path_silhouette,
+)
 
 __all__ = ["area_of_applicability"]
